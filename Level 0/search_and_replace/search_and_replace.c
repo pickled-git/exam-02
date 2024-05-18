a889cf77ac46e1adb3b3d67f029078b29d214c28
oprosvir <ol7897@ya.ru>
2024-05-18 21:01:31 +0200

    improved

--- Level 0/search_and_replace/search_and_replace.c
+++ Level 0/search_and_replace/search_and_replace.c
@@ -1,32 +1,20 @@
 #include <unistd.h>
 
-int str_len (char *str)
+void	search_and_replace(char *str, char *search, char *replace)
 {
-    int i = 0;
-    while (*str)
-    {
+	while (*str)
+	{
+		if (*str == *search)
+            *str = *replace;
+        write(1, str, 1);
         str++;
-        i++;
-    }
-    return i;
+	}
 }
 
-void search_and_replace(char *str, char *search, char *replace)
+int	main(int argc, char *argv[])
 {
-    while (*str)
-    {
-        if (*str == *search)
-            write(1, replace, 1);
-        else
-            write(1, str, 1);
-        str++;
-    }
-
-}
-
-int main (int argc, char *argv[])
-{
-    if(argc == 4 && (str_len(argv[2]) < 2) && (str_len(argv[3]) < 2))
-        search_and_replace(argv[1], argv[2], argv [3]);
-    write(1, "\n", 1);
+	if (argc == 4 && !argv[2][1] && !argv[2][1])
+		search_and_replace(argv[1], argv[2], argv[3]);
+	write(1, "\n", 1);
+	return (0);
 }
